fa5587ac02b61d76c9cece8f9d7c9382be3016b2
Rachel Goode <rgoode92@gmail.com>
2022-03-07 16:36:24 -0500

    final updates

--- ProfileLab/ProfileLab/Program.cs
+++ ProfileLab/ProfileLab/Program.cs
@@ -6,31 +6,33 @@ using System.Threading.Tasks;
 
 
 namespace ProfileLab
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+{ //attempted to use VS code to input the location, but couldnt quite get it. I will continue to practice 
+
+
+    //internal class Program
+    //{
+        //static void Main(string[] args)
+        //{
             //< script >
-            var x = (home.html).getElementById("Find Location");
-            internal class findMe getLocation(string navigator)
-            {
-                if (navigator.geolocation)
-                {
-                    navigator.geolocation.getCurrentPosition(showPosition);
-                }
-                else
-                {
-                    x.innerHTML = "Geolocation is not supported by this browser.";
-                }
-            }
+            //var x = (home.html).getElementById("Find Location");
+           // internal class findMe getLocation(string navigator)
+           // {
+                //if (navigator.geolocation)
+                //{
+                    //navigator.geolocation.getCurrentPosition(showPosition);
+                //}
+                //else
+                //{
+                    //x.innerHTML = "Geolocation is not supported by this browser.";
+                //}
+            //}
 
-            function showPosition(position)
-            {
-                x.innerHTML = "Latitude: " + position.coords.latitude +
-                "<br>Longitude: " + position.coords.longitude;
-            }
+            //function showPosition(position)
+            //{
+                //x.innerHTML = "Latitude: " + position.coords.latitude +
+                //"<br>Longitude: " + position.coords.longitude;
+            //}
 
-        }
-    }
+        //}
+    //}
 }
