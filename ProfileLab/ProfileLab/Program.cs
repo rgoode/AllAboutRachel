16a0a49fca371c01de8d41363c9d55fc5aeeca4c
Rachel Goode <rgoode92@gmail.com>
2022-03-07 11:49:43 -0500

    more updates

---
+++ ProfileLab/ProfileLab/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ProfileLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //< script >
+            var x = (home.html).getElementById("Find Location");
+            internal class findMe getLocation(string navigator)
+            {
+                if (navigator.geolocation)
+                {
+                    navigator.geolocation.getCurrentPosition(showPosition);
+                }
+                else
+                {
+                    x.innerHTML = "Geolocation is not supported by this browser.";
+                }
+            }
+
+            function showPosition(position)
+            {
+                x.innerHTML = "Latitude: " + position.coords.latitude +
+                "<br>Longitude: " + position.coords.longitude;
+            }
+
+        }
+    }
+}
